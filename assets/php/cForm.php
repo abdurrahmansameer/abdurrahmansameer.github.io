aa5459e3f9170cba3fe9e8b6a6aa0ee5d368bfbe
Abdurrahman-Sameer <abdurrahman.sameer6@gmail.com>
2020-06-20 07:43:56 +0530

    Php

---
+++ assets/php/cForm.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Contact Form</title>
+</head>
+
+<body>
+	<main>
+		<p>SEND E-MAIL</p>
+		<form class="contact-form" action="contactform.php" method"post">
+			<input type="text" name="Name" placeholder="Full name">
+			<input type="text" name="mail" placeholder="Your E-mail">
+			<input type="text" name="subject" placeholder="Subject">
+        	<textarea name="Message"  placeholder="Message"></textarea>
+			<button type="submit" name="submit"> SEND&nbsp;</button>
+		</form>
+	</main>
+</body>
+</html>

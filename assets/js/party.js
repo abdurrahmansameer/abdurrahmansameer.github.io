cc0623c0ddff292090f67d3ad9fc5c3f9a107354
Abdurrahman Sameer <63775809+abdurrahmansameer@users.noreply.github.com>
2020-12-07 15:12:36 +0530

    g

---
+++ assets/js/party.js
@@ -0,0 +1,10 @@
+$('.like__btn').on('click', function(){
+  // Check if it's already been clicked
+  if (!$(this).hasClass('like__btn--disabled')) {
+    // Update the number
+    updated_likes = parseInt($('.like__btn span').html()) + 1;
+    $('.like__btn span').html(updated_likes);
+   }
+  // Make btn disabled
+  $(this).attr('disabled', true).addClass('tada');
+});
